40b645c372d751ca2119da76bd940279a4da96ca
Anders Klenke <anders_klenke@icloud.com>
2019-06-22 17:06:18 +0200

    Run Prettier

--- db/migrate/20190621111949_devise_create_users.rb
+++ db/migrate/20190621111949_devise_create_users.rb
@@ -3,13 +3,13 @@
 class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.string :first_name,         null: false
-      t.string :last_name,          null: false
-      t.string :email,              null: false, default: ''
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :email, null: false, default: ''
       t.string :encrypted_password, null: false, default: ''
 
       ## Recoverable
-      t.string   :reset_password_token
+      t.string :reset_password_token
       t.datetime :reset_password_sent_at
 
       t.datetime :remember_created_at
@@ -17,7 +17,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.timestamps null: false
     end
 
-    add_index :users, :email,                unique: true
+    add_index :users, :email, unique: true
     add_index :users, :reset_password_token, unique: true
   end
 end
