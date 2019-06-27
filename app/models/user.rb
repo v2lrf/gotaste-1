693167586114546704eb8d1f4ec94385134984c7
Anders Klenke <anders_klenke@icloud.com>
2019-06-27 11:54:54 +0200

    Add role to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  enum role: { user: 0, admin: 1 }
+
   devise :database_authenticatable,
          :registerable,
          :recoverable,
