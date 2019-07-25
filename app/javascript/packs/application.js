5f395037b09aaec2edd7ec18624088913513cd94
Anders Klenke <anders_klenke@icloud.com>
2019-07-25 20:37:54 +0200

    Fix prettier

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,5 +16,5 @@ require('@rails/activestorage').start()
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-require("trix")
-require("@rails/actiontext")
+require('trix')
+require('@rails/actiontext')
