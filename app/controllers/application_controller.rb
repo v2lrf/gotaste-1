b705f62cc67f667363a0d7349dcda3d2985d1e6d
Anders Klenke <anders_klenke@icloud.com>
2019-09-10 14:30:31 +0200

    WIP

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,16 @@ class ApplicationController < ActionController::Base
   include Pundit
 
   before_action :set_locale
+  before_action :redirect_to_pre_launch_page
   after_action :track_page_view
 
   protected
+
+  def redirect_to_pre_launch_page
+    return if current_user.admin?
+
+    redirect_to root_path
+  end
 
   def track_page_view
     properties = request.path_parameters
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,16 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
+  before_action :render_pre_launch
+
   def index
     @places = Place.where(active: true)
     @events = Event.upcoming.order(:begins_at)
+  end
+
+  private
+
+  def render_pre_launch
+    render 'pre_launch', layout: 'pre_launch'
   end
 end
