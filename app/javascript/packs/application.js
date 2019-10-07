25550d2012374eecd2a5821a5ab3037b40232ac9
Anders Klenke <anders_klenke@icloud.com>
2019-10-07 21:26:57 +0200

    Add Font Awesome via Yarn

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,9 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import { library, dom } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
 import ahoy from 'ahoy.js'
 import LocalTime from 'local-time'
 
@@ -11,6 +14,11 @@ import '../css/application.css'
 require('@rails/ujs').start()
 require('turbolinks').start()
 require('@rails/activestorage').start()
+
+// Font Awespome
+// Kicks off the process of finding <i> tags and replacing with <svg>
+library.add(fas)
+dom.watch()
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
