78945f38f290fc180fe62876e2c3729efef6e748
Anders Klenke <anders_klenke@icloud.com>
2019-10-10 12:02:47 +0200

    Make sitemap valid

--- spec/requests/sitemaps_spec.rb
+++ spec/requests/sitemaps_spec.rb
@@ -3,6 +3,12 @@
 require 'rails_helper'
 
 describe 'Sitemaps', type: :request do
+  before do
+    create :place, category: :bar, active: true
+    create :place, category: :shop, active: true
+    create :event
+  end
+
   describe 'GET index' do
     it 'is a successful request' do
       get sitemap_path(format: :xml)
